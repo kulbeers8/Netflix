adf849cc530252ef7c1b8e8c1804e4901e2fccdd
testing <thakurkulbeersingh@gmail.com>
2023-01-24 21:48:26 +0530

    First Project HTML

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="keywords" content="HTML,Testing"/>
+        <meta name="description" content="Testing purpose to site development"/>
+        <meta name="author" content="Kulbeer Singh"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta http-equiv="refresh" content="30"/>
+        <link rel="stylesheet" href="assests/css/stylesheet.css">
+        <link rel="stylesheet" href="assests/css/bootstrap.min.css"/>
+        <!-- <script rel="text/javascript" href="../js/jquery.slim.min.js"/> -->
+        <title>Netflix</title>
+    </head>
+  <body>
+        
---
+++ index.php
@@ -0,0 +1,39 @@
+<?php include('header.php');?>
+<!-- Header Section With Logo and other details -->
+<section class="header-section">
+    <div class="container">
+         <div class="row">
+              <div class="site-logo">
+              <img src="assests/images/logo.png" alt="website-logo"/>
+              </div>
+               <div class="header-button">
+               <select>
+                 <option value="English">English</option>
+                 <option value="Hindi">Hindi</option>
+              </select>
+                <button name="Sign In">Sign In</button>
+               </div>
+        </div>
+        <div class="header-discription">
+            <h1>Unlimited movies, TV <br>shows and more.</h1>
+            <h2>Watch anywhere. Cancel anytime.</h2>
+            <p>Ready to watch? Enter your email to create or restart your membership.</p>
+            <input type="text" placeholder="Email address"><button>Get Started</button>
+        </div>
+     </div>
+    
+</section>
+
+<!-- Enjoy TV section -->
+<section class="section-tv">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+       <h2>Enjoy on your TV.</h2>
+       <h3>Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more.</h3>
+      </div>
+      <div class="col-md-6">
+
+      </div>  
+  </div>
+</section>
